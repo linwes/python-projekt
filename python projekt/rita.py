cb91aee71a180e022a28def44fbf23b51179165a
linwes <linus.westerback@gmail.com>
2020-03-11 19:10:18 +0100

    senaste ändringarna 03/11

--- python projekt/rita.py
+++ python projekt/rita.py
@@ -1,12 +1,59 @@
+from PIL import ImageTk, Image, ImageDraw
+import PIL
+from tkinter import *
 import json
-import random
-import sqlite3
-from flask import Flask, escape, request, jsonify, render_template
-import requests
 
-app = Flask(__name__)
+width = 800
+height = 400
+center = height//2
+white = (255, 255, 255)
+green = (0,128,0)
 
-djur = ["apa","hund","katt","häst","hammare","sol","snö","gaffel","dörr","ägg","hamster","elefant","snickare","orm","lampa","mus","tangentbord","tallrik","spis","element","stol","bord","vatten","kran","handuk","dator","disktrasa","hörlurar","arm","ben","människa",]
+def save():
+    filename = "static/bild.png"
+    image1.save(filename)
 
-ritaDjur = (random.choice(djur))
-print(ritaDjur)
+def paint(event):
+    # python_green = "#476042"
+    x1, y1 = (event.x - 1), (event.y - 1)
+    x2, y2 = (event.x + 1), (event.y + 1)
+    cv.create_oval(x1, y1, x2, y2, fill="black",width=5)
+    draw.line([x1, y1, x2, y2],fill="black",width=5)
+
+root = Tk()
+
+
+# Tkinter create a canvas to draw on
+cv = Canvas(root, width=width, height=height, bg='white')
+cv.pack()
+
+# PIL create an empty image and draw object to draw on
+# memory only, not visible
+image1 = PIL.Image.new("RGB", (width, height), white)
+draw = ImageDraw.Draw(image1)
+
+# do the Tkinter canvas drawings (visible)
+# cv.create_line([0, center, width, center], fill='green')
+
+cv.pack(expand=YES, fill=BOTH)
+cv.bind("<B1-Motion>", paint)
+
+
+with open('Ritat_ord.json', 'r', encoding='utf8') as ord_file:
+    jord = json.load(ord_file)
+
+test = str(jord)
+
+message = Label(root, text=f'rita följande ord: {test}')
+message.pack(side=BOTTOM)
+
+# do the PIL image/draw (in memory) drawings
+# draw.line([0, center, width, center], green)
+
+# PIL image can be saved as .png .jpg .gif or .bmp file (among others)
+# filename = "my_drawing.png"
+# image1.save(filename)
+button=Button(text="save",command=save)
+button.pack()
+root.mainloop()
+
---
+++ python projekt/server.py
@@ -0,0 +1,33 @@
+import json
+import random
+from flask import Flask, escape, request, jsonify, render_template
+import requests
+
+with open('Ritat_ord.json', 'r', encoding='utf8') as ord_file:
+    jord = json.load(ord_file)
+
+test = str(jord)
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    return render_template('index.html')
+
+@app.route('/', methods=['post'])
+def skrivUt():
+    användare = request.form.get('fNamn','svar')
+    
+    fNamn = användare
+    if fNamn == test:
+        djuret = ("du hade rätt!")
+        
+    else: 
+        djuret = ("du hade fel försök ingen")
+
+    return render_template('index.html', fNamn=fNamn, djuret=djuret, hej=test)
+
+if __name__ :
+    app.debug = True
+    app.run(host= '192.168.1.3', port=5080)
